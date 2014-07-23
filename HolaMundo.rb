fab9bd40797e9fbbaefd80a5db3e0d327ca0a680
Leonardo Barros <nickolback@gmail.com>
2014-07-22 22:18:09 -0300

    Conversão de tipos

---
+++ HolaMundo.rb
@@ -0,0 +1,14 @@
+class HolaMundo
+	def initialize()
+	end
+	def saluda()
+		nombre = "1"
+		nombre = nombre.to_f
+		valorDos = 2
+		puts nombre + valorDos
+	end
+end
+
+objeto = HolaMundo.new()
+objeto.saluda
+gets()
