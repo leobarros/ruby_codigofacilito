4ae119b2bf048ed05068045e3b33c7cc86a2b434
Leonardo Barros <nickolback@gmail.com>
2014-08-03 22:19:36 -0300

    Uso do print e do puts para saída de dados.
    Uma explicação simples do uso do print e do puts.

---
+++ Saida_de_dados.rb
@@ -0,0 +1,19 @@
+
+class HolaMundo	
+	def initialize()
+	end
+
+	def saluda()
+
+		print "Hola Mundo"
+		print " Ola Mundo\n"
+
+		puts "Ola Mundo"
+		puts "Hola Mundo"
+
+	end
+end
+
+objeto = HolaMundo.new()
+objeto.saluda
+gets()
